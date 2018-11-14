d5cb05d0c02da1b9cc7b432c0699c5f2a3a6bf9a
xatalakula <haiprime30@gmail.com>
2018-11-15 00:08:38 +0700

    chinh sua lai man hinh theo doi hoat dong, them 2 joystick vao man hinh dieu khien drone

---
+++ app/src/main/java/com/example/noname/ttnm12/ManuallyOperateActivity.java
@@ -0,0 +1,14 @@
+package com.example.noname.ttnm12;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class ManuallyOperateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manually_operate);
+    }
+}
--- app/src/main/java/com/example/noname/ttnm12/OperationDroneFrag.java
+++ app/src/main/java/com/example/noname/ttnm12/OperationDroneFrag.java
@@ -1,18 +1,20 @@
 package com.example.noname.ttnm12;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class OperationDroneFrag extends Fragment {
-
+    Button btnControl;
 
     public OperationDroneFrag() {
         // Required empty public constructor
@@ -23,7 +25,16 @@ public class OperationDroneFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_operation_drone, container, false);
+        View view = inflater.inflate(R.layout.fragment_operation_drone, container, false);
+        btnControl = (Button) view.findViewById(R.id.btn_control);
+        btnControl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),ManuallyOperateActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 
 }
